618169788d0bec504a122354cbe1fbdcfb01e6aa
jan.vlcinsky <jan.vlcinsky@gmail.com>
2014-08-23 01:38:56 +0200

    Added README.rst and NEWS.rst
            modified:   MANIFEST.in
            new file:   NEWS.rst
            new file:   README.rst
            modified:   setup.py

---
+++ NEWS.rst
@@ -0,0 +1,12 @@
+News
+====
+
+0.1dev - 2014-08-23
+-------------------
+
+- initial version
+- includes py.test and tox test suite
+- jar files for jing2009
+- included jar files for:
+  1. Jing version 20091111
+  2. Trang version 20091111
--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(".")
+README = open(os.path.join(here, "README.rst")).read()
+NEWS = open(os.path.join(here, "NEWS.rst")).read()
 
 setup(
     name="jingtrang",
     version="0.1dev",
-    description="Wrapping jing and trang RELAX NG into Python script",
+    description="Wrapping jing and trang RELAX NG tools into Python script",
+    long_description=README + "\n" + NEWS,
     author="Jan Vlcinsky",
     author_email="jan.vlcinsky@tamtamresearch.com",
     packages=find_packages(),
