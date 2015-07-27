ada429b46b132cd66e141da974aaab24b51849c3
Jan Vlčinský <jan.vlcinsky@tamtamresearch.com>
2015-07-27 20:25:36 +0200

    setup.py rewritten to `pbr`
            modified:   .gitignore
            deleted:    NEWS.rst
            new file:   setup.cfg
            modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,3 @@
-from setuptools import setup, find_packages
-import os
+from setuptools import setup
 
-here = os.path.abspath(".")
-README = open(os.path.join(here, "README.rst")).read()
-NEWS = open(os.path.join(here, "NEWS.rst")).read()
-
-setup(
-    name="jingtrang",
-    version="0.1dev",
-    description="Wrapping jing and trang RELAX NG tools into Python script",
-    long_description=README + "\n" + NEWS,
-    author="Jan Vlcinsky",
-    author_email="jan.vlcinsky@tamtamresearch.com",
-    packages=find_packages(),
-    entry_points={
-        "console_scripts": [
-            "pyjing=jingtrang:jing",
-            "pytrang=jingtrang:trang"
-        ]
-    },
-    zip_safe=False,
-    package_data={"jingtrang": ["*.jar"]}
-)
+setup(setup_requires=["pbr"], pbr=True)
